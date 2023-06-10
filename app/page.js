8bc269963e0d8f0383885980a23cc96d1ca52b2e
Roman <didoshakr@ukr.net>
2023-06-10 14:27:21 +0300

    1

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Product from "../components/Product"
-import { getAllProducts } from "./api1/data"
+// import { getAllProducts } from "./api1/data"
+import { getAllProducts } from "./data"
 
 export default async function Home() {
   const products = await getAllProducts()
@@ -12,7 +13,7 @@ export default async function Home() {
   return (
     // <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 mt-48">
     <section className="flex flex-col space-y-12 pb-44">
-      <h1 className="text-5xl font-bold text-center">ПРОПОЗИЦІЇ ДНЯ</h1>
+      <h1 className="text-5xl font-bold text-center">Наші вироби</h1>
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {products.map((product) => (
           <Product key={product.id} product={product} />
