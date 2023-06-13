b0ba5e2db0a950865ecb5f520a612ad50ec284a8
Roman <didoshakr@ukr.net>
2023-06-13 17:36:37 +0300

    2

--- components/header/HeaderTape.js
+++ components/header/HeaderTape.js
@@ -11,7 +11,7 @@ const HeaderTape = () => {
     // <div className="headerTape-section">
     <div className="fixed z-50 top-0 px-2 w-full h-12 md:h-6 flex flex-col md:flex-row justify-start md:justify-between bg-headMenuText text-white text-sm ">
       <span className="relative flex items-center justify-between px-2">
-        <span className="flex items-center justify-between">
+        <span className="flex items-center justify-between text-sm">
           <IconPhone width={iconSize} height={iconSize} colorFill="white" /> + 38(050-8580704/068-7892306)
         </span>
         <a className="px-2" href="https://www.facebook.com/profile.php?id=100017742340573">
