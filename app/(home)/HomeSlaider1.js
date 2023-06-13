8de0ba6ff6e51df15428e279a0934afa05e61d4c
Roman <didoshakr@ukr.net>
2023-06-13 16:14:23 +0300

    22

--- app/(home)/HomeSlaider1.js
+++ app/(home)/HomeSlaider1.js
@@ -1,12 +1,16 @@
 import Image from "next/image"
+import BVorota0 from "../../public/images/home/DBVorota0.jpg"
 
 export default function HomeSlaider1() {
   return (
     <>
-      <div className="relative w-full h-60 md:h-[580px]">
+      {/* <div className="relative w-full h-60 md:h-[580px]"> */}
+      <div className="relative w-full h-[580px]">
         {/* <div className="relative w-screen h-60 md:h-[580px]"> */}
-        <div className="absolute inset-0">
+        <div className="absolute w-full h-full">
           <Image src="/images/home/DBVorota0.jpg" alt="background image" fill />
+          {/* Авторозмір - імпорт свого зображення*/}
+          {/* <Image src={BVorota0} alt="background image" /> */}
         </div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full">
           <h1 className="text-4xl font-bold top-4 text-center text-white"> Вироби з нержавійки на замовлення</h1>
