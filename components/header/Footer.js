7fc43f6c7d4fb04340041a23be5806fac52f6be5
Roman <didoshakr@ukr.net>
2023-06-13 08:37:31 +0300

    2

--- components/header/Footer.js
+++ components/header/Footer.js
@@ -10,35 +10,36 @@ const Footer = () => {
   return (
     <section className=" p-2 relative">
       {/* <div className="footer-mouse"> */}
-      {/* <div className="absolute">
-        <a href="#" className="footer-mouse-icon">
+      <div className="absolute">
+        {/* <a href="#" className="footer-mouse-icon"> */}
+        <a href="#" className="w-[60px] h-[60px] bor">
           <div className="footer-mouse-wheel">
             <span className="ion-ios-arrow-up">
               <IconShevronUp width="14" height="14" />
             </span>
           </div>
         </a>
-      </div> */}
+      </div>
       {/* </div> */}
       {/* <div className="footer-context-row"> */}
-      <div className="flex flex-row flex-auto justify-between items-center">
+      <div className="flex flex-row flex-auto justify-between items-start mx-2">
         {/* <div className="footer-context-item-col"> */}
         <div className="relative w-full flex flex-col justify-start items-start text-left">
-          <h2 className="">DBogdan</h2>
-          <p>Нержавійка - стійка до корозії в атмосфері й агресивних середовищах сталь із вмістом хрому.</p>
+          <h2 className="flex flex-row justify-center items-center text-headMenuText">DBogdan</h2>
+          {/* <p>В соціальних мережах</p> */}
           <ul className="flex justify-between items-center">
             <li>
               <a href="https://www.facebook.com/profile.php?id=100017742340573">
-                <span className="icon-social">
-                  <IconFacebook width="26" height="26" />
-                </span>
+                {/* <span className="icon-social"> */}
+                <IconFacebook width="26" height="26" />
+                {/* </span> */}
               </a>
             </li>
           </ul>
         </div>
 
         <div className="relative w-full flex flex-col justify-start items-start text-left">
-          <h2 className="flex flex-row justify-center items-center">Є запитання?</h2>
+          <h2 className="px-2 mx-4 flex flex-row justify-center items-center text-headMenuText">Є запитання?</h2>
           <ul>
             <li className="px-4 mx-2 flex flex-row justify-start items-center">
               <IconMapMarker width="15" height="15" colorFill="#000" colorStroke="#000" />
