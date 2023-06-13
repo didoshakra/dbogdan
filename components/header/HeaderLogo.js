43fcc7188ae2341f934e98ad8db4c6546b4237df
Roman <didoshakr@ukr.net>
2023-06-13 17:49:38 +0300

    2

--- components/header/HeaderLogo.js
+++ components/header/HeaderLogo.js
@@ -9,7 +9,8 @@ const HeaderLogo = () => {
     // <div className="flex items-center px-4 md:px-12 py-2 justify-begin ">
     <div className=" flex justify-begin items-center  min-w-[200px] px-0 md:px-2  ">
       <Link href="/">
-        <Image src="/images/home/StainlessSteelBoat.png" width={100} height={20} alt="Logo" />
+        {/* <Image src="/images/home/StainlessSteelBoat.png" width={100} height={20} alt="Logo" /> */}
+        <Image src="/images/home/StainlessSteelBoat-oval.png" width={80} height={20} alt="Logo" />
       </Link>
       <Link href="/">
         {/* <div className="flex items-center px-4 md:px-12 py-2 justify-begin w-full z-50 shadow">DBogdan</div> */}
