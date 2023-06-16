97ab4494ce94fa8d9066859a32f0860dd30b38bb
Roman <didoshakr@ukr.net>
2023-06-16 13:35:42 +0300

    1

--- components/header/Footer.js
+++ components/header/Footer.js
@@ -58,8 +58,10 @@ const Footer = () => {
             </li>
             <li className="px-4  flex flex-row justify-start items-center text-sm">
               <IconPhone width="15" height="15" colorFill={colorIcon} />
-              <span className="px-2">+ 38(068-7832306)</span>
-              <a href="tel:+380985554433"></a>
+              {/* <span className="px-2">+ 38(068-7832306)</span> */}
+              <a className="px-2" href="tel:+380985554433">
+                + 38(068-7832306)
+              </a>
             </li>
             <li className="px-4  flex flex-row justify-start items-center text-sm">
               <IconMail width="15" height="15" icon colorFill={colorIcon} colorFill1={colorIcon} />
